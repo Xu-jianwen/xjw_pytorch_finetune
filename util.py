ac9debb5c9918361757891215cd4fa1f9bd20b07
Xu-jianwen <18310606913@163.com>
2021-09-26 21:33:09 +0800

    添加冻结BN操作

--- util.py
+++ util.py
@@ -33,6 +33,12 @@ def collate_fn(batch):
     return torch.stack(imgs, dim=0), labels
 
 
+def set_bn_eval(m):
+    classname = m.__class__.__name__
+    if classname.find("BatchNorm") != -1:
+        m.eval()
+
+
 def tsne_feature_visualization(name, features, n_components):
     features_tsne = TSNE(n_components=n_components).fit_transform(features)
     return features_tsne
